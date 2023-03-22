e91da3996bfe555958cdcc00e9d3edbe8a1b2edd
Alexander Krivács Schrøder <alexschrod@gmail.com>
2023-03-22 09:51:07 +0100

    fix: replace unsafe code with dyn borrow trick
    This keeps the `MapTupleExtensions` working as before, but now without unsafe shenanigans. Based on an idea/code from Daniel Henry-Mantilla (https://github.com/danielhenrymantilla).

--- src/map_extensions.rs
+++ src/map_extensions.rs
@@ -1,88 +1,239 @@
 //! Various [`HashMap`] and [`BTreeMap`] extensions
 
 use paste::paste;
+use std::borrow::Borrow;
+use std::cmp::Ordering;
 use std::collections::{BTreeMap, HashMap};
-use std::hash::Hash;
-use std::mem::ManuallyDrop;
+use std::hash::{Hash, Hasher};
+//use std::mem::ManuallyDrop;
 
-macro_rules! map_tuple_extensions {
+macro_rules! tuple_borrowing {
     ($($ty:ident) +) => {
-
         paste! {
-            /// Because there is no generic safe way to make a reference to a tuple of values
-            /// when what you have is individual references to values without having to
-            /// to clone said values, a process that seems wasteful and unnecessary when all
-            /// you are using them for is for looking up values in a map, we do some unsafe
-            /// (but sound¹!) trickery to turn individual references into a reference to a tuple
-            /// and then use that to look up a value.
-            ///
-            /// ¹ Both according to my own understanding and by Miri
-            pub trait [< MapTupleExtensions $($ty)+ >] <$($ty,)+ V> {
-                /// Look up a value in the map with the tuple key based on the individual
-                /// `entry*` references
-                #[allow(nonstandard_style)]
-                #[allow(clippy::too_many_arguments)]
-                fn get_by_tuple_entries(&self, $(paste!{ [<entry $ty>] }: &$ty),+) -> Option<&V>;
+            trait [< HashTupleBorrow $($ty)+ >]<$($ty,)+> {
+                fn tuple(&self) -> ($(&$ty,)+);
+            }
+            impl<$($ty,)+> [< HashTupleBorrow $($ty)+ >]<$($ty,)+> for ($($ty,)+) {
+                #[allow(non_snake_case)]
+                fn tuple(&self) -> ($(&$ty,)+) {
+                    let ($([< entry $ty >],)+) = self;
+                    ($([< entry $ty >],)+)
+                }
+            }
+            impl<$($ty,)+> [< HashTupleBorrow $($ty)+ >]<$($ty,)+> for ($(&'_ $ty,)+) {
+                #[allow(non_snake_case)]
+                fn tuple(&self) -> ($(&$ty,)+) {
+                    let ($([< entry $ty >],)+) = self;
+                    ($([< entry $ty >],)+)
+                }
+            }
+            impl<'r, $($ty,)+> Hash for dyn 'r + [< HashTupleBorrow $($ty)+ >]<$($ty,)+>
+            where
+                $($ty: 'r + Hash,)+
+            {
+                fn hash<H: Hasher>(&self, h: &mut H) {
+                    self.tuple().hash(h);
+                }
+            }
+            impl<'r, $($ty,)+> PartialEq for dyn 'r + [< HashTupleBorrow $($ty)+ >]<$($ty,)+>
+            where
+                $($ty: 'r + Eq ,)+
+            {
+                fn eq(self: &Self, other: &Self) -> bool {
+                    self.tuple() == other.tuple()
+                }
+            }
+            impl<'r, $($ty,)+> Eq for dyn 'r + [< HashTupleBorrow $($ty)+ >]<$($ty,)+>
+            where
+                $($ty: 'r + Eq ,)+
+            {
+            }
+            impl<'r, $($ty,)+> Borrow<dyn 'r + [< HashTupleBorrow $($ty)+ >]<$($ty,)+>> for ($($ty,)+)
+            where
+                $($ty: 'r + Eq + Hash,)+
+            {
+                fn borrow(&self) -> &(dyn 'r + [< HashTupleBorrow $($ty)+ >]<$($ty,)+>) {
+                    self
+                }
             }
 
-            impl<$($ty,)+ V> [< MapTupleExtensions $($ty)+ >] <$($ty,)+ V> for HashMap<($($ty,)+), V>
+
+            trait [< OrdTupleBorrow $($ty)+ >]<$($ty,)+> {
+                fn tuple(&self) -> ($(&$ty,)+);
+            }
+            impl<$($ty,)+> [< OrdTupleBorrow $($ty)+ >]<$($ty,)+> for ($($ty,)+) {
+                #[allow(non_snake_case)]
+                fn tuple(&self) -> ($(&$ty,)+) {
+                    let ($([< entry $ty >],)+) = self;
+                    ($([< entry $ty >],)+)
+                }
+            }
+            impl<$($ty,)+> [< OrdTupleBorrow $($ty)+ >]<$($ty,)+> for ($(&'_ $ty,)+) {
+                #[allow(non_snake_case)]
+                fn tuple(&self) -> ($(&$ty,)+) {
+                    let ($([< entry $ty >],)+) = self;
+                    ($([< entry $ty >],)+)
+                }
+            }
+            impl<'r, $($ty,)+> PartialEq for dyn 'r + [< OrdTupleBorrow $($ty)+ >]<$($ty,)+>
+            where
+                $($ty: 'r + Ord ,)+
+            {
+                fn eq(self: &Self, other: &Self) -> bool {
+                    self.tuple() == other.tuple()
+                }
+            }
+            impl<'r, $($ty,)+> Eq for dyn 'r + [< OrdTupleBorrow $($ty)+ >]<$($ty,)+>
+            where
+                $($ty: 'r + Ord ,)+
+            {
+            }
+            impl<'r, $($ty,)+> PartialOrd for dyn 'r + [< OrdTupleBorrow $($ty)+ >]<$($ty,)+>
+            where
+                $($ty: 'r + Ord,)+
+            {
+                fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                    self.tuple().partial_cmp(&other.tuple())
+                }
+            }
+            impl<'r, $($ty,)+> Ord for dyn 'r + [< OrdTupleBorrow $($ty)+ >]<$($ty,)+>
+            where
+                $($ty: 'r + Ord,)+
+            {
+                fn cmp(&self, other: &Self) -> Ordering {
+                    self.tuple().cmp(&other.tuple())
+                }
+            }
+            impl<'r, $($ty,)+> Borrow<dyn 'r + [< OrdTupleBorrow $($ty)+ >]<$($ty,)+>> for ($($ty,)+)
+            where
+                $($ty: 'r + Ord,)+
+            {
+                fn borrow(&self) -> &(dyn 'r + [< OrdTupleBorrow $($ty)+ >]<$($ty,)+>) {
+                    self
+                }
+            }
+
+            /// The method provided by this extension trait allows you to look up values
+            /// in a map with a tuple key — i.e.
+            #[doc = ::std::concat!("`", ::std::stringify!(($($ty),+)), "`")]
+            /// — by utilizing a trick where you essentially have
+            #[doc = ::std::concat!("`Borrow<", ::std::stringify!(($(&$ty),+)), "> for ", ::std::stringify!(($($ty),+)), "`")]
+            /// letting us look up a tuple key with owned values using a tuple of the same
+            /// types with borrowed values.
+            pub trait [< MapTupleExtensions $($ty)+ >]<$($ty,)+ Val> {
+                /// Look up a value in a map using a tuple of owned values as a key by using
+                /// a tuple of borrowed values.
+                ///
+                /// # Examples
+                ///
+                /// Because the trait implementations are macro generated, the example will only
+                /// use a specific tuple size, but it works the same for all tuples up to arity
+                /// 10.
+                ///
+                /// ```
+                /// use std::collections::HashMap;
+                /// use ilyvion_util::map_extensions::MapTupleExtensionsTUW;
+                ///
+                /// let mut hash_map = HashMap::new();
+                /// hash_map.insert((16_i32, 32_u8, String::from("Hello, world!")), "first");
+                /// hash_map.insert((8_i32, 16_u8, String::from("Bye, world!")), "second");
+                ///
+                /// assert_eq!(Some(&"first"), hash_map.get_by_tuple((&16, &32, &String::from("Hello, world!"))));
+                /// assert_eq!(Some(&"second"), hash_map.get_by_tuple((&8, &16, &String::from("Bye, world!"))));
+                /// ```
+                ///
+                /// ```
+                /// use std::collections::BTreeMap;
+                /// use ilyvion_util::map_extensions::MapTupleExtensionsTUW;
+                ///
+                /// let mut tree_map = BTreeMap::new();
+                /// tree_map.insert((16_i32, 32_u8, String::from("Hello, world!")), "first");
+                /// tree_map.insert((8_i32, 16_u8, String::from("Bye, world!")), "second");
+                ///
+                /// assert_eq!(Some(&"first"), tree_map.get_by_tuple((&16, &32, &String::from("Hello, world!"))));
+                /// assert_eq!(Some(&"second"), tree_map.get_by_tuple((&8, &16, &String::from("Bye, world!"))));
+                /// ```
+                fn get_by_tuple(&self, ($([< entry_ $ty:lower >],)+): ($(&$ty,)+)) -> Option<&Val> {
+                    self.get_by_tuple_entries($([< entry_ $ty:lower >],)+)
+                }
+
+                /// Look up a value in a map using a tuple of owned values as a key by using
+                /// borrowed values.
+                ///
+                /// # Examples
+                ///
+                /// Because the trait implementations are macro generated, the example will only
+                /// use a specific tuple size, but it works the same for all tuples up to arity
+                /// 10.
+                ///
+                /// ```
+                /// use std::collections::HashMap;
+                /// use ilyvion_util::map_extensions::MapTupleExtensionsTUW;
+                ///
+                /// let mut hash_map = HashMap::new();
+                /// hash_map.insert((16_i32, 32_u8, String::from("Hello, world!")), "first");
+                /// hash_map.insert((8_i32, 16_u8, String::from("Bye, world!")), "second");
+                ///
+                /// assert_eq!(Some(&"first"), hash_map.get_by_tuple_entries(&16, &32, &String::from("Hello, world!")));
+                /// assert_eq!(Some(&"second"), hash_map.get_by_tuple_entries(&8, &16, &String::from("Bye, world!")));
+                /// ```
+                ///
+                /// ```
+                /// use std::collections::BTreeMap;
+                /// use ilyvion_util::map_extensions::MapTupleExtensionsTUW;
+                ///
+                /// let mut tree_map = BTreeMap::new();
+                /// tree_map.insert((16_i32, 32_u8, String::from("Hello, world!")), "first");
+                /// tree_map.insert((8_i32, 16_u8, String::from("Bye, world!")), "second");
+                ///
+                /// assert_eq!(Some(&"first"), tree_map.get_by_tuple_entries(&16, &32, &String::from("Hello, world!")));
+                /// assert_eq!(Some(&"second"), tree_map.get_by_tuple_entries(&8, &16, &String::from("Bye, world!")));
+                /// ```
+                #[allow(clippy::too_many_arguments)]
+                fn get_by_tuple_entries(&self, $([< entry_ $ty:lower >]: &$ty,)+) -> Option<&Val>;
+            }
+            impl<$($ty,)+ Val> [< MapTupleExtensions $($ty)+ >]<$($ty,)+ Val> for HashMap<($($ty,)+), Val>
             where
                 ($($ty,)+): Eq + Hash,
+                $($ty: Eq + Hash,)+
             {
                 #[allow(nonstandard_style)]
                 #[inline]
-                fn get_by_tuple_entries(&self,  $(paste!{ [<entry $ty>] }: &$ty),+) -> std::option::Option<&V> {
-                    let k = [< make_tuple_key$($ty)+ >]($(paste!{ [<entry $ty>] }),+);
-                    self.get(&k)
+                fn get_by_tuple_entries(&self, $([< entry $ty >]: &$ty,)+) -> Option<&Val> {
+                    let k: &dyn [< HashTupleBorrow $($ty)+ >]<$($ty,)+> = &($([< entry $ty >],)+);
+                    self.get(k)
                 }
             }
-
-            impl<$($ty,)+ V> [< MapTupleExtensions $($ty)+ >] <$($ty,)+ V> for BTreeMap<($($ty,)+), V>
+            impl<$($ty,)+ Val> [< MapTupleExtensions $($ty)+ >]<$($ty,)+ Val> for BTreeMap<($($ty,)+), Val>
             where
                 ($($ty,)+): Ord,
+                $($ty: Ord,)+
             {
                 #[allow(nonstandard_style)]
                 #[inline]
-                fn get_by_tuple_entries(&self,  $(paste!{ [<entry $ty>] }: &$ty),+) -> std::option::Option<&V> {
-                    let k = [< make_tuple_key$($ty)+ >]($(paste!{ [<entry $ty>] }),+);
-                    self.get(&k)
+                fn get_by_tuple_entries(&self, $([< entry $ty >]: &$ty,)+) -> Option<&Val> {
+                    let k: &dyn [< OrdTupleBorrow $($ty)+ >]<$($ty,)+> = &($([< entry $ty >],)+);
+                    self.get(k)
                 }
             }
-
-            #[allow(unsafe_code)]
-            #[allow(nonstandard_style)]
-            #[allow(clippy::too_many_arguments)]
-            #[inline]
-            fn [< make_tuple_key$($ty)+ >] <$($ty,)+>($(paste!{ [<entry $ty>] }: &$ty),+) -> ManuallyDrop<($($ty,)+)> {
-                let k = unsafe {
-                    // Copy the type values of the `entry*`s so that they are adjacent.
-                    // This makes a tuple backed by the same data as the `entry*`s.
-                    ($(std::ptr::read(paste!{ [<entry $ty>] }),)+)
-                };
-
-                // Make sure not to drop the values, even if we panic. The caller
-                // or whoever actually owns the `entry*`s will be responsible
-                // for dropping them.
-                ManuallyDrop::new(k)
-            }
         }
-    }
+    };
 }
 
-map_tuple_extensions!(T U);
+tuple_borrowing!(T U);
 
-map_tuple_extensions!(T U W);
+tuple_borrowing!(T U W);
 
-map_tuple_extensions!(T U W X);
+tuple_borrowing!(T U W X);
 
-map_tuple_extensions!(T U W X Y);
+tuple_borrowing!(T U W X Y);
 
-map_tuple_extensions!(T U W X Y Z);
+tuple_borrowing!(T U W X Y Z);
 
-map_tuple_extensions!(T U W X Y Z A);
+tuple_borrowing!(T U W X Y Z A);
 
-map_tuple_extensions!(T U W X Y Z A B);
+tuple_borrowing!(T U W X Y Z A B);
 
-map_tuple_extensions!(T U W X Y Z A B C);
+tuple_borrowing!(T U W X Y Z A B C);
 
-map_tuple_extensions!(T U W X Y Z A B C D);
+tuple_borrowing!(T U W X Y Z A B C D);
